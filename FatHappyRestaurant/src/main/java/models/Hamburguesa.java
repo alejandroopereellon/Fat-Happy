73f1338359c0f463cf2e66a468dd21d3e657319e
aleja <aleja@Alejandro>
2025-03-30 16:11:11 +0200

    CLASE Hamburguesa
    - Añadida clase Hamburguesa con herencia de Producto
    - Relaciones @ManyToMany con Ingrediente y Extra
    - Constructor completo y constructor vacío para Hibernate
    - Getters, setters y método toString personalizado

--- FatHappyRestaurant/src/main/java/models/Hamburguesa.java
+++ FatHappyRestaurant/src/main/java/models/Hamburguesa.java
@@ -1,14 +1,9 @@
 package models;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 /**
  * Esta clase permite generar el objeto hamburguesa que hereda de
@@ -29,45 +24,13 @@ public class Hamburguesa extends Producto {
 	@Column(name = "opcionMenu")
 	private boolean opcionMenu;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ingrediente1", referencedColumnName = "codigo")
-	private Ingrediente ingrediente1;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "hamburguesa_ingredientes", joinColumns = @JoinColumn(name = "codigo_hamburguesa"), inverseJoinColumns = @JoinColumn(name = "codigo_ingrediente"))
+	private List<Ingrediente> listaIngredientes;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ingrediente2", referencedColumnName = "codigo")
-	private Ingrediente ingrediente2;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ingrediente3", referencedColumnName = "codigo")
-	private Ingrediente ingrediente3;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ingrediente4", referencedColumnName = "codigo")
-	private Ingrediente ingrediente4;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ingrediente5", referencedColumnName = "codigo")
-	private Ingrediente ingrediente5;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ingrediente6", referencedColumnName = "codigo")
-	private Ingrediente ingrediente6;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ingrediente7", referencedColumnName = "codigo")
-	private Ingrediente ingrediente7;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "extra1", referencedColumnName = "codigo")
-	private Extra extra1;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "extra2", referencedColumnName = "codigo")
-	private Extra extra2;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "extra3", referencedColumnName = "codigo")
-	private Extra extra3;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "hamburguesa_extras", joinColumns = @JoinColumn(name = "codigo_hamburguesa"), inverseJoinColumns = @JoinColumn(name = "codigo_extra"))
+	private List<Extra> listaExtras;
 
 	/**
 	 * constructor para generar las hamburguesas de productos con todos sus
@@ -93,37 +56,17 @@ public class Hamburguesa extends Producto {
 	 * @param imagenProductoOriginal la imagen original del producto
 	 * @param opcionMenu             indica si la hamburguesa es parte de un menu
 	 *                               (true o false)
-	 * @param ingrediente1           id del primer ingrediente de la hamburguesa
-	 * @param ingrediente2           id del segundo ingrediente de la hamburguesa
-	 * @param ingrediente3           id del tercer ingrediente de la hamburguesa
-	 * @param ingrediente4           id del cuarto ingrediente de la hamburguesa
-	 * @param ingrediente5           id del quinto ingrediente de la hamburguesa
-	 * @param ingrediente6           id del sexto ingrediente de la hamburguesa
-	 * @param ingrediente7           id del septimo ingrediente de la hamburguesa
-	 * @param extra1                 id del primer extra de la hamburguesa
-	 * @param extra2                 id del segundo extra de la hamburguesa
-	 * @param extra3                 id del tercer extra de la hamburguesa
 	 */
 
 	public Hamburguesa(int codigo, String nombreProducto, String categoria, String tipoProducto, BigDecimal precioVenta,
 			BigDecimal costeEmpresa, boolean productoActivo, boolean productoPromocionado, boolean opcionDescuento,
 			String imagenProducto64, String imagenProducto128, String imagenProductoOriginal, boolean opcionMenu,
-			Ingrediente ingrediente1, Ingrediente ingrediente2, Ingrediente ingrediente3, Ingrediente ingrediente4,
-			Ingrediente ingrediente5, Ingrediente ingrediente6, Ingrediente ingrediente7, Extra extra1, Extra extra2,
-			Extra extra3) {
+			List<Ingrediente> listaIngredientes, List<Extra> extras) {
 		super(codigo, nombreProducto, categoria, tipoProducto, precioVenta, costeEmpresa, productoActivo,
 				productoPromocionado, opcionDescuento, imagenProducto64, imagenProducto128, imagenProductoOriginal);
 		this.opcionMenu = opcionMenu;
-		this.ingrediente1 = ingrediente1;
-		this.ingrediente2 = ingrediente2;
-		this.ingrediente3 = ingrediente3;
-		this.ingrediente4 = ingrediente4;
-		this.ingrediente5 = ingrediente5;
-		this.ingrediente6 = ingrediente6;
-		this.ingrediente7 = ingrediente7;
-		this.extra1 = extra1;
-		this.extra2 = extra2;
-		this.extra3 = extra3;
+		this.listaIngredientes = listaIngredientes;
+		this.listaExtras = extras;
 	}
 
 	// Constructor sin argumentos para hibernate
@@ -140,114 +83,31 @@ public class Hamburguesa extends Producto {
 		this.opcionMenu = opcionMenu;
 	}
 
-	public Ingrediente getIngrediente1() {
-		return ingrediente1;
+	public List<Ingrediente> getListaIngredientes() {
+		return listaIngredientes;
 	}
 
-	public Ingrediente getIngrediente2() {
-		return ingrediente2;
-	}
-
-	public Ingrediente getIngrediente3() {
-		return ingrediente3;
-	}
-
-	public Ingrediente getIngrediente4() {
-		return ingrediente4;
-	}
-
-	public Ingrediente getIngrediente5() {
-		return ingrediente5;
-	}
-
-	public Ingrediente getIngrediente6() {
-		return ingrediente6;
-	}
-
-	public Ingrediente getIngrediente7() {
-		return ingrediente7;
-	}
-
-	public Extra getExtra1() {
-		return extra1;
-	}
-
-	public Extra getExtra2() {
-		return extra2;
-	}
-
-	public Extra getExtra3() {
-		return extra3;
-	}
-
-	@Override
-	public String toString() {
-
-		StringBuilder datos = new StringBuilder(getNombreProducto());
-
-		// Añadimos la información de los ingredientes
-		if (ingrediente1 != null) {
-			datos.append("\n\tIngrediente 1: ").append(ingrediente1.toString());
-		}
-		if (ingrediente2 != null) {
-			datos.append("\n\tIngrediente 2: ").append(ingrediente2.toString());
-		}
-		if (ingrediente3 != null) {
-			datos.append("\n\tIngrediente 2: ").append(ingrediente3.toString());
-		}
-		if (ingrediente4 != null) {
-			datos.append("\n\tIngrediente 2: ").append(ingrediente4.toString());
-		}
-		if (ingrediente5 != null) {
-			datos.append("\n\tIngrediente 2: ").append(ingrediente5.toString());
-		}
-		if (ingrediente6 != null) {
-			datos.append("\n\tIngrediente 2: ").append(ingrediente6.toString());
-		}
-		if (ingrediente7 != null) {
-			datos.append("\n\tIngrediente 2: ").append(ingrediente7.toString());
-		}
-
-		if (extra1 != null) {
-			datos.append("\n\tExtra 5: ").append(extra1.toString());
-		}
-		if (extra2 != null) {
-			datos.append("\n\tExtra 5: ").append(extra2.toString());
-		}
-		if (extra3 != null) {
-			datos.append("\n\tExtra 5: ").append(extra3.toString());
-		}
-
-		// Añadimos la información de los extras
-		if (extra1 != null) {
-			datos.append("\n\tExtra 1: ").append(extra1.toString());
-		}
-		if (extra2 != null) {
-			datos.append("\n\tExtra 2: ").append(extra2.toString());
-		}
-		if (extra3 != null) {
-			datos.append("\n\tExtra 3: ").append(extra3.toString());
-		}
-
-		return datos.toString();
+	public List<Extra> getExtras() {
+		return listaExtras;
 	}
 
 	// toString
-//	@Override
-//	public String toString() {
-//		String datos = super.toString();
-//
-//		// Mostramos los ingredientes
-//		for (Ingrediente ing : ingrendientes) {
-//			if (!ing.isActivo()) {
-//				datos = datos + ing.toString();
-//			}
-//		}
-//		// Mostramos los extras
-//		for (Extra ext : extras) {
-//			if (ext.getCantidadExtra() > 0) {
-//				datos = datos + ext.toString();
-//			}
-//		}
-//		return datos;
+	@Override
+	public String toString() {
+		String datos = super.toString();
+
+		// Mostramos los ingredientes
+		for (Ingrediente ing : listaIngredientes) {
+			if (ing.isActivo()) {
+				datos = datos + ing.toString() + System.lineSeparator();
+			}
+		}
+		// Mostramos los extras
+		for (Extra ext : listaExtras) {
+			if (ext.getCantidadExtra() > 0) {
+				datos = datos + ext.toString() + System.lineSeparator();
+			}
+		}
+		return datos;
+	}
 }
